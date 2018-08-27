7e3f53d3501413c55f02b49e2013eb2f80ee1826
ibavli <ibrahim.bavli35@gmail.com>
2018-08-27 22:45:31 +0300

    Facade Design Pattern

--- FacadeDesignPattern/Program.cs
+++ FacadeDesignPattern/Program.cs
@@ -10,6 +10,48 @@ namespace FacadeDesignPattern
     {
         static void Main(string[] args)
         {
+            Bilgisayar bilgisayar = new Bilgisayar();
+            
         }
     }
+    public class Islemci
+    {
+        public void IslemciIs1()
+        {
+
+        }
+        public void IslemciIs2()
+        {
+
+        }
+    }
+    public class RAM
+    {
+        public void RamIslem1()
+        {
+
+        }
+        public void RamIslem2()
+        {
+
+        }
+    }
+    public class Harddisk
+    {
+        public void HarddiskIslem1()
+        {
+
+        }
+        public void HarddiskIslem2()
+        {
+
+        }
+    }
+    public class Bilgisayar
+    {
+        public Islemci Islemci { get; set; }
+        public RAM Ram { get; set; }
+        public Harddisk Harddisk { get; set; }
+    }
+
 }
